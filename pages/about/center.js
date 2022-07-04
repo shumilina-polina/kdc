9e05fb648daa82086d047804a84de26ffdedf640
Artem <artemmorozov1308@gmail.com>
2022-07-04 22:40:22 +0300

    created footer navigation

--- pages/about/center.js
+++ pages/about/center.js
@@ -1,7 +1,17 @@
+import CenterBlock from "Components/CenterBlock/CenterBlock";
+import Footer from "Components/Footer/Footer";
 import Header from "Components/Header/Header";
 
 const CenterPage = () => {
-  return <Header />;
+  return(
+    <>
+      <Header />
+      
+      <CenterBlock />
+
+      <Footer />
+    </>
+  )
 };
 
 export default CenterPage;
