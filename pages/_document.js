a2ccb585da7f0ed03746fa9b900dfc39d1770319
Artem <artemmorozov1308@gmail.com>
2022-07-14 01:07:05 +0500

    added fonts fixed skeleton and NaN

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Karla&family=Oswald:wght@500&display=swap" rel="stylesheet"/>
+        </Head>
         <body>
           <Main />
           <div id="modal__root"></div>
