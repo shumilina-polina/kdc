67873e79697f6d9c498c1f652c7cf5841e3d7193
Artem <artemmorozov1308@gmail.com>
2022-08-01 13:27:40 +0300

    added ability to send mail

--- Components/JoinCollectiveWindow/JoinCollectiveWindow.js
+++ Components/JoinCollectiveWindow/JoinCollectiveWindow.js
@@ -1,4 +1,6 @@
 import cn from "classnames";
+import { useState } from "react";
+import apiService from "services/apiService";
 import useHtmlDecode from "shared/hooks/useHtmlDecode";
 
 import Button from "UI/Button/Button";
@@ -11,9 +13,21 @@ const JoinCollectiveWindow = (props) => {
     collective: { title, trend, price, thumbnail },
   } = props;
 
+  const [request, setRequest] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const buttonClickHandler = () => {
+    apiService.postEmailJoinCollective(name, email, phone).then(() => setRequest(true))
+  }
+
   return (
     <div className={s.container}>
-      <div className={s.info}>
+      {!request ? (
+        <>
+        <div className={s.info}>
         <img src={thumbnail} className={s.poster} />
         <div className={s.table}>
           <p className={cn(s.bold, s.title)}>{useHtmlDecode(title)}</p>
@@ -55,11 +69,15 @@ const JoinCollectiveWindow = (props) => {
         </div>
       </div>
       <div className={s.fields}>
-        <CustomInput className={s.input} label="Как к вам обращаться?" />
-        <CustomInput className={s.input} label="Ваша почта" />
-        <CustomInput className={s.input} label="Номер телефона" />
-        <Button className={s.button}>Оставить заявку</Button>
+        <CustomInput className={s.input} label="Как к вам обращаться?" onChange={(e) => setName(e.target.value)} />
+        <CustomInput className={s.input} label="Ваша почта" onChange={(e) => setEmail(e.target.value)} />
+        <CustomInput className={s.input} label="Номер телефона" onChange={(e) => setPhone(e.target.value)} />
+        <Button className={s.button} onClick={buttonClickHandler}>Оставить заявку</Button>
       </div>
+        </>
+      ) : (
+        <div>Запрос успешно отправлен</div>
+      )}
     </div>
   );
 };
