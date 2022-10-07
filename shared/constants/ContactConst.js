894a0506c6e81bc2917e10ffd791a0829f2c0125
Polina Shumilina <shumilina246@gmail.com>
2022-10-07 11:07:03 +0300

    fix: phone

--- shared/constants/ContactConst.js
+++ shared/constants/ContactConst.js
@@ -1,7 +1,7 @@
 export const ContactConst = {
   adressMain: "Московский проспект, д. 152",
   adressSecondary: "Варшавская улица д. 98",
-  phone: "+7 (932) 241-54-82",
+  phone: "+7 (812) 241-54-82",
   secondPhone: "+7 (812) 373-29-91",
   email: "kdcmoskovskiy@mail.ru",
   secondEmail: "spb.kdc98@gmail.com",
